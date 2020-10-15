10b0c1996e074eb40ddd6352cf5872bf86a9bdca
Arijit Mondal <arijit.mondal@fairprice.com.sg>
2020-10-15 16:56:23 +0800

    initial commit

---
+++ pages/accounts.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Account() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <h1 className={styles.title}>Account</h1>
+    </div>
+  );
+}
